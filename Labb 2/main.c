0ae2c28c563f5c27e8533f27f4ae01f4859e7660
ivario123 <ivajns-9@student.ltu.se>
2020-09-23 11:09:16 +0200

    Finnished lab 2 i think

--- Labb 2/main.c
+++ Labb 2/main.c
@@ -140,35 +140,93 @@ void main(){
 }
 #endif
 #if 1
-long Pow(int a, int b){
-    if(b==0)
+/// Computes the value of Base^Exponent
+/// \param Base is the base of the power of computation
+/// \param Exponent is the exponent of the power of computation
+/// \return returns Base^Exponent no matter if the exponent is positive or negative
+long double Pow(int Base, int Exponent){
+    long double ret = 1;
+    if(Exponent==0)
         return 1;
-    if(b ==1)
-        return a;
-    return a*Pow(a,b-1);
+    if(Exponent<0)
+        for(int i = 0; i < -Exponent;++i)
+            ret/=Base;
+    else
+        for(int i = 0; i < Exponent;++i)
+            ret*=Base;
+    return ret;
 }
-void sizeA(int A){
 
-    int Default = 4;
-    int sizeX = 210;
-    int sizeY = 297;
-    if(A<Default){
-        sizeX*=Pow(2,Default-A);
-        sizeY*=Pow(2,Default-A);
-    }
-    else if(A>Default){
-        sizeX/=Pow(2,A-Default);
-        sizeY/=Pow(2,A-Default);
-    }
-    printf("A paper of size A%i is %i by %i mm\n",A,sizeX,sizeY);
+
+/// Function that computes the size of a given Ax size paper
+/// \param BaseSize is the size of an A4 Paper
+/// \param Base Probably 2 but this would work for anny relation not just half or double size as long as the relation is an integer
+/// \param Exp Is the size that we want to calculate
+/// \return  Returns the size of an A(Exp) Paper
+long double calc(int BaseSize[],int Base,int Exp)
+{
+    int Size[] = {(int)(BaseSize[0]*Pow(Base,Exp)),(int)(BaseSize[1]*Pow(Base,Exp))};
+    if(Size[0] == 0)
+        return printf("A paper of size A%i is %i*%i^%i by %i*%i^%i mm\n", 4 - Exp, BaseSize[0],Base,Exp,BaseSize[1],Base,Exp);
+    return printf("A paper of size A%i is %i by %i mm\n",4-Exp,Size[0],Size[1]);
 }
+/// Function that checks if a given string is a positive numerical string
+/// \param Str is the string that will be checked
+/// \param Constraint Is the length constraint of the string
+/// \return Returns 1 if the string is a number and 0 if not
+int IsNum(char *Str,int Constraint)
+{
+    int len = strlen(Str);
+    if(len>Constraint)
+    {
+        printf("You have to enter a positive number smaller than 999\n");
+        return 0;
+    }
+    for(int i = 0; i < len; ++i)
+    {
+        //if the char is outside of the valid ascii range then return 0
+        if(Str[i]>58 && Str[i]< 48){
+            printf("You must enter a positive number\n");
+            return 0;
+        }
+    }
+    //if all chars are accepted return 1
+    return 1;
+}
+/// Function that converts a given string to a number and assures that the string is numerical
+/// \param Str is the string that is to be converted to an integer
+/// \param Constraints is the length constraints, set this to max int if you dont want to constrain the size of the number
+/// \return returns the integer value contained in the string if the string is numerical
+/// otherwise returns the value -1
+long Atoi(char *Str,int Constraints){
+    long Ret = 0;
+    int len = strlen(Str);
+    if(IsNum(Str,Constraints)==1) {
+        //48 är ASCII värdet för 0 och alla heltal följer.
+        //Eftersom att vi har kontrollerat att strängen är tal så kan vi bara subtrahera 48
+        //för att konvertera till heltal
+        for (int i = 0; i < len; ++i)
+            Ret += (Str[i] - 48) * Pow(10, len - i - 1);
+        return Ret;
+    }
+    return -1;
+}
+/// Main function, will loop indefinitely unless Q is the size argument
 void main(){
-    char *str[256];
-    while(1==1){
+    //loopar så att man kan kolla mer än 1 tal
+    while(1){
+        char *str[256];
+        //Kan ta bort denna printen om ni vill men tyckte att det var snyggare så
+        printf("--------------------------------------------------\n");
         printf("Which size do you want to calculate?\nA");
         scanf("%s",&str);
-        int i = atoi(str);
-        sizeA(i);
+        if(str[0]=='Q')
+            return;
+        int i = Atoi(str,4);
+        if(i!=-1 && i <=1000) {
+            long size[2] = {210, 297};
+            calc(size, 2, 4 - i);
+        }
     }
 }
 #endif
